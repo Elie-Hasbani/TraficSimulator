e42c3b8d31b6e96af1872bd5dc6d4e9ad3e11ff1
Elie-Hasbani <elie.hasbani498@gmail.com>
2025-01-21 15:22:16 +0100

    ok

--- test.cpp
+++ test.cpp
@@ -3,9 +3,25 @@
 
 using namespace std;
 
+void f(int * test){cout<<"toto"<<endl;};
+void f(const int* test){cout<<"titi"<<endl;}
+
+void f(int & test){cout<<"toto"<<endl;};
+void f(const int & test){cout<<"titi"<<endl;}
+
 int main() {
 
-    int ok = 1;
-    cout << !ok<<endl;
+int i = 2;
+int *ptr1 = &i;
+const int* ptr2 = &i;
+
+//f(ptr1);
+//f(ptr2);
+
+int j = 2;
+const int k = 3;
+
+f(j);
+f(k);
 
 }
